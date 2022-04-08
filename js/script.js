29c27c5feee2dd4dc2d39e344be64b01c14a2784
gideonalves <gideonam20@gmail.com>
2022-04-08 09:02:09 -0300

    Formulario adicionado

--- js/script.js
+++ js/script.js
@@ -54,7 +54,6 @@ const galeriaContainer = document.querySelector('.bicicleta-imagens')
 function trocarImagem(event) {
     const img = event.currentTarget
     const media = matchMedia('(min-width: 1000px)').matches
-    console.log(media)
     if (media) {
         galeriaContainer.prepend(img)
     }
